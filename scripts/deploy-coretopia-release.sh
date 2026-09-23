e1eebf6de8b27ff5b246f43a8eb663ee4b455402
pranesh <pranesh@coretopia.com>
2026-09-23 09:09:50 +0530

    mount the fuseki dataset config on dev, comment said it should be there but the actual line was missing

--- scripts/deploy-coretopia-release.sh
+++ scripts/deploy-coretopia-release.sh
@@ -236,7 +236,8 @@ branch_web_remote_build() {
     ontology-plugin-service/pom.xml ontology-plugin-service/src/ \
     ontology-reasoner-worker/pom.xml ontology-reasoner-worker/src/ \
     "${dockerfiles[@]}" docker-compose.remote-build.yml \
-    docker-compose.de.ec2.yml docker-compose.production.yml docker-compose.r6i-xlarge.yml)
+    docker-compose.de.ec2.yml docker-compose.production.yml docker-compose.r6i-xlarge.yml \
+    fuseki-config.ttl)
   if [[ " ${SERVICES[*]} " == *" web "* ]]; then
     local webapp_env="$ROOT/ontology-vscode-extension/webview-src/.env.production"
     {
